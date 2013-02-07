8e1df2f1a65770d4a8f0d4cb93fb7a7a034025a1
Robin Heinel <robin.heinel@gmail.com>
2013-02-07 20:07:43 +0100

    html wrapper added

---
+++ html/js/functions.js
@@ -0,0 +1,80 @@
+// Common Functions
+
+/* Gibt URL des aktuellen Dokumentes zurueck.
+*/
+
+function getLocation() {
+	return document.location.href;
+}
+
+function urlParam( name )
+{
+	name = name.replace(/[\[]/,"\\\[").replace(/[\]]/,"\\\]");
+	var regexS = "[\\?&]"+name+"=([^&#]*)";
+	var regex = new RegExp( regexS );
+	var results = regex.exec( getLocation() );
+	if( results == null )
+		return "";
+	else
+		return results[1];
+}
+
+function maxWindow() {
+	if (isWindowTooSmall())
+	{
+		var msg = "An error occurred. The resolution of your browser window is to low.\n" +
+			"This application needs a minimum size of 1024x768px.\n\n" +
+			"Es ist ein Fehler aufgetreten. Die Gr��e ihres Browserfensters ist zu gering.\n" +
+			"Diese Anwendung ben�tigt eine Mindestgr��e von 1024x768px.";
+
+		alert(msg);
+
+		window.moveTo(0,0);
+		if (document.all)
+		{
+			top.window.resizeTo(screen.availWidth,screen.availHeight);
+		}
+		else
+		{
+			if (document.layers || document.getElementById)
+			{
+				if (top.window.outerHeight < screen.availHeight || top.window.outerWidth < screen.availWidth)
+				{
+					top.window.outerHeight = screen.availHeight;
+					top.window.outerWidth = screen.availWidth;
+				}
+			}
+		}
+	}
+}
+
+function isWindowTooSmall() {
+	var viewportwidth = 0;
+	var viewportheight = 0;
+
+	if (typeof(window.innerWidth) == 'number')
+	{
+		// the more standards compliant browsers (mozilla/netscape/opera/IE7) use window.innerWidth and window.innerHeight
+
+		viewportwidth = window.innerWidth,
+		viewportheight = window.innerHeight
+	}
+	else if (document.documentElement && (document.documentElement.clientWidth || document.documentElement.clientHeight))
+	{
+				viewportwidth = document.documentElement.clientWidth,
+				viewportheight = document.documentElement.clientHeight
+	}
+	else if (document.body && (document.body.clientWidth || document.body.clientHeight))
+	{
+		// older versions of IE
+
+				viewportwidth = document.body.clientWidth,
+				viewportheight = document.body.clientHeight
+	}
+
+	if (viewportwidth < 860 || viewportheight < 560)
+		return true;
+
+	return false;
+}
+
